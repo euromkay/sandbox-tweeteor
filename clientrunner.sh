f2fa4a83f89c0ae618fa8be6002e7790a07df199
Thomas Garcia <thomasjgarcia1@gmail.com>
2015-04-01 08:40:11 -0700

    Clienrunner now reads the number of rows/columns from server.conf, so it does not need to be edited if the number of windows is changed.

--- clientrunner.sh
+++ clientrunner.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
-for x in `seq 0 1`; do
-    for y in `seq 0 1`; do
+maxX=`awk '/win_per_row/ {print $3 - 1}' < server.conf`
+maxY=`awk '/win_per_col/ {print $3 - 1}' < server.conf`
+for x in `seq 0 $maxX`; do
+    for y in `seq 0 $maxY`; do
         python2 client.py $x $y &
     done
 done
