14eed58f3fc86e509b341bba42db41e951634d23
Thomas Garcia <thomasjgarcia1@gmail.com>
2015-06-19 09:16:28 -0700

    Clients now fail gracefully

--- client.py
+++ client.py
@@ -17,7 +17,6 @@ class Client(Thread):
 
     def __init__(self, address, coords, exit):
         Thread.__init__(self, name='Client')
-        pygame.init()
         pygame.display.set_caption(
             str(coords[0]) + "-" + str(coords[1]))
         self.coords = self.x, self.y = coords
@@ -32,27 +31,24 @@ class Client(Thread):
         self.sock.send('ACK')
 
     def run(self):
-        while True:
-            # Exits if window was closed
-            if exit.is_set():
-                self.sock.close()
-                pygame.quit()
-                sys.exit()
-            length = int(self.sock.recv(1024))
-            self.sock.send('ACK')  # Tells server that it got the length
-            msg = self.sock.recv(2048)
-            # Calls recv multiple times to get the entire message
-            while len(msg) < length:
-                msg += self.sock.recv(2048)
-            self.sock.send('done')
-            msg = msg[0:length]  # cut off the extra bytes
-            if msg == 'exit':
-                self.sock.close()
-                exit.set()
-                return
-            tweets = json.loads(msg, object_hook=decode_tweet)
-            self.update_screen(tweets)
-            pygame.display.update()
+        try:
+            while not exit.is_set():
+                length = int(self.sock.recv(1024))
+                self.sock.send('ACK')  # Tells server that it got the length
+                msg = self.sock.recv(2048)
+                # Calls recv multiple times to get the entire message
+                while len(msg) < length:
+                    msg += self.sock.recv(2048)
+                self.sock.send('done')
+                msg = msg[0:length]  # cut off the extra bytes
+                if msg == 'exit':
+                    break
+                tweets = json.loads(msg, object_hook=decode_tweet)
+                self.update_screen(tweets)
+                pygame.display.update()
+        finally:
+            self.sock.close()
+            exit.set()
 
     def update_screen(self, tweets):
         self.window.fill(TWITTER_BLUE)
@@ -80,21 +76,20 @@ if __name__ == "__main__":
     # Uses coords from file if in auto mode, otherwise asks user for coords
     coords = (int(sys.argv[1]), int(sys.argv[2]))
     exit = Event()  # Event for coordinating shutdown
-    client = Client(address, coords, exit)
-    client.start()
-    while True:
-        # Exits if Client thread already exited
-        if exit.is_set():
-            pygame.quit()
-            sys.exit()
-        # Exits if window was closed, and tells Client thread to close
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit.set()
-                break
-        # This else block executes as long as the for loop did not break
-        # (e.g. the program is not shutting down)
-        else:
-            pygame.time.wait(10)
-            continue
-        break  # Only executes if earlier break was called
+    try:
+        pygame.init()
+        client = Client(address, coords, exit)
+        client.start()
+        while not exit.is_set():
+            # Exits if window was closed, and tells Client thread to close
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit.set()
+                    client.join()
+                    break
+            # This else block executes as long as the for loop did not break
+            # (e.g. the program is not shutting down)
+            else:
+                pygame.time.wait(10)
+    finally:
+        pygame.quit()
